6a50d106ba5de600c8730e7672adbb13c11b21c3
DOAcoder <dadahonovorifjon808@gmail.com>
2023-10-19 11:27:55 +0500

    ok

--- src/app/followers/page.jsx
+++ src/app/followers/page.jsx
@@ -1,57 +1,67 @@
 "use client";
 import { useEffect, useState } from "react";
 import useUsersApi from "@api/Users";
-import UserItem from "@ui/UserList/UserItem";
+import UserItem from "../../components/UI/UserList/UserItem";
 
 const page = () => {
   const [followers, setFollowers] = useState([]);
- const [step, setStep] =useState(1)
- const [pbtn, psetBtn] =useState(false)
- const [nbtn, nsetBtn] = useState(false);
-
+  const [step, setStep] = useState(1);
+  const [pbtn, psetBtn] = useState(false);
+  const [nbtn, nsetBtn] = useState(false);
 
   useEffect(() => {
-
-    if(step<=1){
-    psetBtn(true)
-
+    if (step <= 1) {
+      psetBtn(true);
     }
     useUsersApi.paginateFollowers(step).then((res) => {
-   
-      if(res.status == 200){
-          setFollowers(res?.data);
-          nsetBtn(false)
+      if (res.status == 200) {
+        setFollowers(res?.data);
+        nsetBtn(false);
       }
-      
     });
   }, [step]);
 
-const next=()=>{
-  setStep(step+1)
-  nsetBtn(true)
-  psetBtn(false)
-}
-const prev=()=>{
-  setStep(step-1)
-  psetBtn(true)
-}
+  const next = () => {
+    setStep(step + 1);
+    nsetBtn(true);
+    psetBtn(false);
+  };
+  const prev = () => {
+    setStep(step - 1);
+    psetBtn(true);
+  };
 
   return (
     <div className="p-2">
       <ul>
-        {
-          followers?.length ? followers?.map((user)=>{
-            return
-            <li>
-              <UserItem user={user} />
-            </li>
-          }): <h1>NOT FOUND!</h1>
-        }
+        {followers?.length ? (
+          followers?.map((user) => {
+            return (
+              <li key={user?.id}>
+                <UserItem user={user} />
+              </li>
+            );
+          })
+        ) : (
+          <h1>NOT FOUND!</h1>
+        )}
       </ul>
 
       <div className="flex items-center gap-[50px]">
-        <button disabled ={pbtn} className="border border-gray-700 py-2 px-4 rounded-md " onClick={()=> prev()}>prev</button>
-        <button disabled ={nbtn} className="border border-gray-700 py-2 px-4 rounded-md " onClick={()=>next()}>next</button>
+        <button
+          disabled={pbtn}
+          className="border border-gray-700 py-2 px-4 rounded-md "
+          onClick={() => prev()}
+        >
+          prev
+        </button>
+        <button
+          disabled={nbtn}
+          className="border border-gray-700 py-2 px-4 rounded-md "
+          onClick={() => next()}
+        >
+          next
+        </button>
       </div>
     </div>
   );
--- src/service/Users/index.js
+++ src/service/Users/index.js
@@ -2,8 +2,8 @@ import api from "../axios";
 
 const useUsersApi = {
  saerchProfile: async (data="mukhriddin-dev") => api.get(`/users/${data}`, { cache: 'force-cache' }),
-  allfollowers: async (data = "mukhriddin-dev") => api.get(`/users/${data}/followers?per_page=2000`),
-  paginateFollowers: async (data = "mukhriddin-dev",num=1) => api.get(`mukhriddin-dev/${data}/followers?page=${num}`),
+  // allfollowers: async (data = "mukhriddin-dev") => api.get(`/users/${data}/followers?per_page=2000`),
+  paginateFollowers: async (num = 1) => api.get(`/users/${"mukhriddin-dev"}/followers?page=${num}`),
   following: async (data = "DOAcoder") => api.get(`/users/${data}/following`),
   repositories: async (data = "DOAcoder") => api.get(`/users/${data}/repos`),
   stars: async (data = "DOAcoder") => api.get(`/users/${data}/starred`),
